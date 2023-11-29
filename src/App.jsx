7bbc6ad0bb6fe48100b8f228e37c3bcf88448650
Shivanktyagi07 <shivanktyagi976@gmail.com>
2023-11-29 20:34:30 +0530

    setup of login

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import './App.css'
 import HomePage from './Pages/HomePages'
 import AboutUs from './Pages/Aboutus'
 import NotFound from './Pages/NotFound'
-import Signup from './Pages/Signup'
+import Signup from './Pages/SignUp'
+import Login from './Pages/Login'
+
 
 
 
@@ -18,7 +20,8 @@ function App() {
       <Route path = "/" element={<HomePage/>}></Route>
       <Route path = "/about" element={<AboutUs/>}></Route>
       <Route path = "/signup" element={<Signup/>}></Route>
-     
+      <Route path = "/login" element={<Login/>}></Route>
+      
       <Route path="*" element={<NotFound />}></Route>
       
     </Routes>
